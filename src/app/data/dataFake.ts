556caf6c2a042b9073fd266932f02ab6dfa54f38
Debfront <deborah_monteiro12@hotmail.com>
2023-09-24 15:36:08 -0300

    Implementações

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,35 @@
 export const dataFake = [
     {
     "id": "1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "Marvel Studios anuncia novo filme do Homem de Ferro",
-    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdKomPox9qW3Y6qPqg0DLGRkCF7IrPeRUrrQ&usqp=CAU"
+    "title": " Heróis classicos",
+    "description": "Spirit- Criado pelo cartunista Will Eisner em 1940, Denny Colt era um detetive policial que foi dado como morto durante uma missão. Tex Willer foi criado em 1948, por Gian Luigi Bonelli e Aurelio Gallepini, em uma série de revistas americanas.", 
+    "photoCover": "https://blogger.googleusercontent.com/img/a/AVvXsEgdy_jQdJFURo0N2nOyqAV3Uxd5jA42SZ6k2uS9e7CAERu3Ag6BcjikpGiOOopABgCkqW23UlmgRUTEIe9CIVm9TZoIGkU65yhA683cGXz5kXQSizUaG7u_IMFR4-ZNgd_hfKE9nYpQaqb_TXOX-nFjDVxH5nsOBURHO7Kl8NkKq33IeEnPXf2K7LfY=s16000"
     },
     {
         "id": "2",
         "title": "Nova Temporada de Spy x Family",
-        "description": "",
+        "description": "Novas aventuras da Anya Estelar chegam em 07 de outubro",
         "photoCover": "https://muramasa.com.br/wp-content/uploads/2022/05/spy-family.png"
+    },
+
+    {
+        "id": "3",
+        "title": " TOP 10 Livros de Fantasia",
+        "description": "Os 31 melhores livros de fantasia que você não pode deixar de ler. A Rainha Vermelha. Autora: Victoria Aveyard. ...As Crônicas de Nárnia. Autor: C. S. Lewis. ...Duna. Autor: Frank Herbert. ...O Lar da Srta. Peregrine para Crianças Peculiares. ... Trono de Vidro. Autora: Sarah J. ... Os Garotos Corvos (A Saga dos Corvos) ...O Hobbit. ...Sombra e Ossos.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfhfGWYnX2zMfQO5EGl7uJ7A4cacs3QRZ-lg&usqp=CAU"
+    },
+
+    {
+        "id": "4",
+        "title": "Novo jogo do Alan Wake",
+        "description": "Alan Wake II é um próximo jogo de survival horror desenvolvido pela Remedy Entertainment e publicado pela Epic Games Publishing. Uma sequência de Alan Wake, o jogo está programado para ser lançado para PlayStation 5, Windows e Xbox Series X/S em 17 de outubro de 2023.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuoG0wOTFNyS4OaFVJ_ZjSp-7v1caBNbSNtA&usqp=CAU"
+    },
+    {
+        "id": "5",
+        "title": "Aprenda de maneira fácil GIT e Github",
+        "description": "GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. Ele permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribuam em projetos privados e/ou Open Source de qualquer lugar do mundo.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRayv-7xg_2pzKdXq6tA2b7dV1iBu86fqwPwQ&usqp=CAU"
     }
+
 ]
