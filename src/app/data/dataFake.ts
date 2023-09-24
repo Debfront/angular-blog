1135457f527e7fc89a74b2eb30064c97a4a1e291
Debfront <deborah_monteiro12@hotmail.com>
2023-09-24 12:12:03 -0300

    content created

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-    "id": 1,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel Studios anuncia novo filme do Homem de Ferro",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdKomPox9qW3Y6qPqg0DLGRkCF7IrPeRUrrQ&usqp=CAU"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "2",
+        "title": "Nova Temporada de Spy x Family",
+        "description": "",
+        "photoCover": "https://muramasa.com.br/wp-content/uploads/2022/05/spy-family.png"
     }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -6,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0RxBhJI_GH6asXfjawFRUYiiLBU3MGmBcBA&usqp=CAU"
-  contentTitle:string ="NOTICIA EXEMPLO"
-  contentDescription:string ="lorem inpsum"
-  constructor() { }
+  photoCover:string = ""
+  contentTitle:string =""
+  contentDescription:string =""
+  private id:string | null = "0"
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
   }
 
+   setValuesToComponent(id:string | null){
+      const result = dataFake.filter(article => article.id == id)[0]
+      
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoCover = result.photoCover
+   }
 }
